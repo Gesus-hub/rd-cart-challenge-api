7dce480d31103af425a33134a4f1c19afe535e5e
Vinicius Guima <vinicius.guimaraes@nowmed.com.br>
2024-11-21 17:42:02 -0300

    install discard

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Cart < ApplicationRecord
+  include Discard::Model
+
   validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  include Discard::Model
+
   validates :name, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
