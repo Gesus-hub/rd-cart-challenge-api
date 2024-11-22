c6e3aba13bce87c964593f7fbf92cceea8705004
Vinicius Guima <vinicius.guimaraes@nowmed.com.br>
2024-11-22 20:20:01 -0300

    Add Product model with validations and associations, and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,6 @@
 class Product < ApplicationRecord
   include Discard::Model
 
-  has_many :cart_items, class_name: "Cart::Item", dependent: :destroy
-  has_many :carts, through: :cart_items
-
   validates :name, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
